116d24212a3bfa0a843f599ee8184c1c28fe65b4
NuraGuru <taganov_ainur@mail.ru>
2024-01-26 14:15:18 +0300

    add commit testing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>lorem</p>
+        <p>lorem</p>
         <a
           className="App-link"
           href="https://reactjs.org"
